835f55dca0616a78f526b29740cc7c3a14609458
A1exStanis <134505123+A1exStanis@users.noreply.github.com>
2024-08-21 12:40:16 +0300

    Compleat Task 1 (Create and add new module)

---
+++ tutorials/estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Not Real Estate',
+    'version': '1.0.0',
+    'category': 'Sales/CRM',
+    'sequence': 5,
+    'summary': 'Testing creation',
+    'description': "",
+    'website': 'https://www.odoo.com/',
+    'depends': [
+        'base_setup'
+    ],
+    'application': True,
+}
