116797467ede44ea50d63f489ac6c35c0a0ef9ae
A1exStanis <134505123+A1exStanis@users.noreply.github.com>
2024-08-26 16:30:38 +0300

    Complete Task 7 (add computed fields and onchanges)

--- tutorials/estate/models/estate_property_offer.py
+++ tutorials/estate/models/estate_property_offer.py
@@ -1,5 +1,5 @@
-from odoo import models, fields
-
+from odoo import models, fields, api
+from datetime import timedelta
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -13,6 +13,28 @@ class EstatePropertyOffer(models.Model):
         default=None)
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
+    validity = fields.Integer('Validity (days)', default=7)
+    date_deadline = fields.Date(
+        string='Date Deadline',
+        compute='_compute_date_deadline',
+        inverse='_inverse_date_deadline'
+    )
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(days=record.validity)
+            else:
+                record.date_deadline = fields.Date.context_today(record) + timedelta(days=record.validity)
+
+    @api.onchange('validity', 'create_date')
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(days=record.validity)
+            else:
+                record.date_deadline = fields.Date.context_today(record) + timedelta(days=record.validity)
 
     def copy(self, default=None):
         if default is None:
